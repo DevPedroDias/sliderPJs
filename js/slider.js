07d6503b0083b5cbdaf689ac1748e0a002601fa5
Pedro Dias <pedroelpidiocdias@gmail.com>
2020-06-12 16:31:58 -0300

    styles alterations ok

--- js/slider.js
+++ js/slider.js
@@ -1,49 +1,62 @@
 class Slider {
     constructor(autoload, elements, time) {
-        this.autoload = autoload;
-        this.element = elements;
-        this.count = elements.length;
-        this.contador = 0;
-        this.time = time;
+        this.autoload = autoload; // set if slide will roll by itself
+        this.element = elements; // witch element contains the slides
+        this.count = elements.length; //number of slides
+        this.contador = 0; //set current slide
+        this.time = time; // time to roll the slide
         this.init()
     }
     init() {
-
+        // will start slider in first slide based in your class
         document.getElementById('slide_0' + this.contador).classList.add('on')
         document.getElementById('prev_slide').addEventListener("click", this.prevSlide.bind(this), false)
         document.getElementById('next_slide').addEventListener("click", this.nextSlide.bind(this), false)
-        if(this.autoload) {
+        if (this.autoload) {
             this.initAutoload();
         }
     }
     nextSlide() {
-        document.getElementById('slide_0' + this.contador).classList.remove('on')
-        if (this.contador < (this.count - 1)) {
-            this.contador = this.contador + 1;
-        } else {
-            this.contador = 0;
-        }
-        document.getElementById('slide_0' + this.contador).classList.add('on')
-        
-    }
-    prevSlide() {
-        document.getElementById('slide_0' + this.contador).classList.remove('on')
-        if (this.contador > 0) {
-            this.contador = this.contador - 1;
-        } else {
-            this.contador = this.count - 1;
-        }
-        document.getElementById('slide_0' + this.contador).classList.add('on')
-    }
-    initAutoload() {
-        setInterval(() => {
+        // pass to the next slide
+        document.getElementById('slide_0' + this.contador).style.marginLeft = '100%'
+        document.getElementById('slide_0' + this.contador).style.transition = '0.5s'
+        setTimeout(() => {
             document.getElementById('slide_0' + this.contador).classList.remove('on')
+
+            document.getElementById('slide_0' + this.contador).style.marginLeft = '0'
+            document.getElementById('slide_0' + this.contador).style.transition = '0'
             if (this.contador < (this.count - 1)) {
                 this.contador = this.contador + 1;
             } else {
                 this.contador = 0;
-            }  
+            }
             document.getElementById('slide_0' + this.contador).classList.add('on')
+        }, 150);
+
+
+    }
+    prevSlide() {
+        // pass to the previous slide
+        document.getElementById('slide_0' + this.contador).style.marginLeft = '-100%'
+        document.getElementById('slide_0' + this.contador).style.transition = '0.5s'
+        setTimeout(() => {
+            document.getElementById('slide_0' + this.contador).classList.remove('on')
+
+            document.getElementById('slide_0' + this.contador).style.marginLeft = '0'
+            document.getElementById('slide_0' + this.contador).style.transition = '0'
+            if (this.contador > 0) {
+                this.contador = this.contador - 1;
+            } else {
+                this.contador = this.count - 1;
+            }
+            document.getElementById('slide_0' + this.contador).classList.add('on')
+        }, 150);
+
+    }
+    initAutoload() {
+        // start auto load slider
+        setInterval(() => {
+            this.nextSlide()
         }, this.time);
     }
 }
